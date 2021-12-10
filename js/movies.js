17df487ceeda8915de8cd5e6ad2300d8d8bd3b0b
Jozelle Mares <jozelle.mares@gmail.com>
2021-12-10 14:05:32 -0600

    added to js

--- js/movies.js
+++ js/movies.js
@@ -3,6 +3,6 @@ const MOVIE_URL = 'https://lively-flash-tumble.glitch.me/movies';
 //get request
 
 let getAllMovies = () => {
-    return fetch(MOVIE_URL).then(resp.json()).catch(err => console.error(err));
+    return fetch(MOVIE_URL).then(resp => resp.json()).catch(err => console.error(err));
 }
 getAllMovies().then(data => console.log(data));
