ed3a037579ce36ea9a5548294f7c7b42d62b955a
Jozelle Mares <jozelle.mares@gmail.com>
2021-12-10 14:55:14 -0600

    added to html

--- js/movies.js
+++ js/movies.js
@@ -1,8 +1,12 @@
-const MOVIE_URL = 'https://lively-flash-tumble.glitch.me/movies';
+$(document).ready(function(){
+    "use strict";
+
+    const MOVIE_URL = 'https://lively-flash-tumble.glitch.me/movies';
 
 //get request
+    let getAllMovies = () => {
+        return fetch(MOVIE_URL).then(resp => resp.json()).catch(err => console.error(err));
+    }
+    getAllMovies().then(data => console.log(data));
 
-let getAllMovies = () => {
-    return fetch(MOVIE_URL).then(resp => resp.json()).catch(err => console.error(err));
-}
-getAllMovies().then(data => console.log(data));
+});
