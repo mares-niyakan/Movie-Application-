8211a298f3dbef49fd727b27874e6ad775e6c030
Jozelle Mares <jozelle.mares@gmail.com>
2021-12-13 10:03:55 -0600

    adding to js

--- js/movies.js
+++ js/movies.js
@@ -2,11 +2,39 @@ $(document).ready(function(){
     "use strict";
 
     const MOVIE_URL = 'https://lively-flash-tumble.glitch.me/movies';
+    const row = $('.row')
+    const loading = $('.loading-container')
+    const btn = $('#addMovie')
+    const deleteOptions = {
+        method: 'DELETE',
+       header:{
+            'Content-Type': 'application/json'
+       }
+   }
 
-//get request
-    let getAllMovies = () => {
-        return fetch(MOVIE_URL).then(resp => resp.json()).catch(err => console.error(err));
-    }
-    getAllMovies().then(data => console.log(data));
+   const fetchData = (delay) => {
+        $('.loading-container').css('margin-top', '150px')
+        loading.toggle('hidden')
+        row.toggle('hidden')
+        setTimeout(function(){
+           fetch(url)
+               .then(res => res.json())
+               .then(data => {
+                   renderHTML(data);
+               })
+
+       })
+   }
+//
+// //get request
+//     let getAllMovies = () => {
+//         return fetch(MOVIE_URL).then(resp => resp.json()).catch(err => console.error(err));
+//     }
+//     getAllMovies().then(data => console.log(data));
+
+
+
+
+
 
 });
